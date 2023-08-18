e6268e15a65a635faecfd7831e93fcb1e312ae43
John Kendall <john.kendall@snyk.io>
2023-08-18 12:00:33 -0400

    updating main.tf to use approved ami

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_vm" {
- ami                       = "ami-04f1014c8adcfa670"
+ ami                       = "ami-00c39f71452c08778"
  instance_type             = "t2.micro"
  disable_api_termination   = true
  iam_instance_profile = "${aws_iam_instance_profile.new_profile.name}"
